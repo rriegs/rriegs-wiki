99b68c0d9f65d996f804c790aff08f79663be415
Ryan Riegel <rriegs@gmail.com>
2018-01-24 02:19:20 -0500

    [#2] Add static file serving via fs-extra

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 "use strict";
 
+const fs = require("fs-extra");
 const { router, get } = require("microrouter");
 
+const getFile = async function (path) {
+    return await fs.readFile("www/" + path, "utf8");
+};
+
 module.exports = router(
-    get("/", function () { return "root"; }),
-    get("/*", function () { return "path"; })
+    get("/", function () { return getFile("index.html"); }),
+    get("/*", function (req) { return getFile(req.params._);} )
 );
