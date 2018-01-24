3ea2f36d0168d3759c58e223ab2d0df40da2b0b0
Ryan Riegel <rriegs@gmail.com>
2018-01-24 16:01:29 -0500

    [#2] Simplify static file serving and detect MIME type

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 "use strict";
 
 const fs = require("fs-extra");
+const mime = require("mime");
 const { router, get } = require("microrouter");
 
-const getFile = async function (path) {
-    return await fs.readFile("www/" + path, "utf8");
+const getPath = async function (req, res) {
+    let filePath = "www/" + (req.params._ || "index.html");
+    let fileType = mime.getType(filePath) || "text/plain";
+    res.setHeader("Content-Type", fileType);
+    return await fs.readFile(filePath);
 };
 
 module.exports = router(
-    get("/", function () { return getFile("index.html"); }),
-    get("/*", function (req) { return getFile(req.params._);} )
+    get("/*", getPath)
 );
