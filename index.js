5275310de2e1edfe73df10174bc9158b978485ed
Ryan Riegel <rriegs@gmail.com>
2018-01-24 19:36:00 -0500

    [#2] Allow PUT and DELETE to the www/data directory
    Routing currently only permits depth-0 paths beneath www/data, so no
    creation of additional subdirectories need be considered.  Further,
    URL path normalization consumes ".." and even "%2E%2E" before routing,
    so it's not possible to escape www/data.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const fs = require("fs-extra");
 const mime = require("mime");
-const { router, get } = require("microrouter");
+const { send, buffer } = require("micro");
+const { router, get, put, del } = require("microrouter");
 
 const getPath = async function (req, res) {
     let filePath = "www/" + (req.params._ || "index.html");
@@ -11,6 +12,21 @@ const getPath = async function (req, res) {
     return await fs.readFile(filePath);
 };
 
+const putData = async function (req, res) {
+    await fs.ensureDir("www/data");
+    let dataPath = "www/data/" + req.params.file;
+    await fs.writeFile(dataPath, await buffer(req));
+    send(res, 200);
+}
+
+const delData = async function (req, res) {
+    let dataPath = "www/data/" + req.params.file;
+    await fs.unlink(dataPath);
+    send(res, 200);
+}
+
 module.exports = router(
-    get("/*", getPath)
+    get("/*", getPath),
+    put("/data/:file", putData),
+    del("/data/:file", delData)
 );
