660a01c04e7671c4efa9c9966d3357f526819aaa
Ryan Riegel <rriegs@gmail.com>
2018-01-30 01:06:43 -0500

    [#3] Expand the front end to use a stub Elm Architecture

--- src/Main.elm
+++ src/Main.elm
@@ -2,5 +2,32 @@ module Main exposing (main)
 
 import Html
 
+type alias Model = String
+
+type Msg
+    = NoOp
+
+init : ( Model, Cmd Msg )
+init =
+    "Hello Elm Architecture!" ! []
+
+view : Model -> Html.Html Msg
+view model =
+    Html.text model
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    model ! []
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+main : Program Never Model Msg
 main =
-    Html.text "Hello world!"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
